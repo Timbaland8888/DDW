d40212e6bb509eb5d651079bb887c1ff27aa1805
www <422033564@qq.com>
2020-10-28 10:55:53 +0800

    修改获取当当自营链接地址的方式

--- app/get_key_html.py
+++ app/get_key_html.py
@@ -7,6 +7,8 @@ import time
 import requests
 import pysnooper
 from lxml import html
+from selenium import webdriver
+
 from tools.console_cmd import *
 from fake_useragent import UserAgent
 
@@ -27,49 +29,64 @@ class DdwBooks():
 
         #过滤页码获取当当网自营的详情
         url = f"{self.web_href}/?key={input_key.strip()}&act=input"
-        headers = {
-            'User-Agent': UserAgent().random
-        }
-        page = requests.get(url=url, headers=headers)
+        #
+        # headers = {
+        #     'User-Agent': UserAgent(verify_ssl=False).random
+        # }
+        # page = requests.get(url=url, headers=headers)
+        #
+        # tree = html.fromstring(page.text)
+        try:
+            # 隐藏浏览器
+            chrome_opts = webdriver.ChromeOptions()
+            chrome_opts.add_argument("--headless")
 
-        tree = html.fromstring(page.text)
+            browser = webdriver.Chrome(options=chrome_opts)
+            # wait = WebDriverWait(browser, 15)
+            browser.get(url)
+            out_html = browser.page_source
+            # print(browser.page_source)
+            tree = html.fromstring(out_html)
 
-        #过滤当当自营的数据提取当当自营的链接
-        go_sort = ''.join(tree.xpath('//*[@id="go_sort"]/div/div[@class="bottom"]/ul//*[@dd_name="当当自营"]//@href'))
-        go_sort_href = self.web_href + go_sort
-        print(f'自营链接地址：{go_sort_href}')
+            #过滤当当自营的数据提取当当自营的链接
+            go_sort = ''.join(tree.xpath('//*[@id="go_sort"]/div/div[@class="bottom"]/ul//*[@dd_name="当当自营"]//@href'))
+            go_sort_href = self.web_href + go_sort
+            print(f'自营链接地址：{go_sort_href}')
 
-        #如果有当当自营的书本信息，则写入数据库
-        if go_sort:
-            go_page = requests.get(url=go_sort_href, headers=headers)
-            go_tree = html.fromstring(go_page.text)
-            bigimge_li_list = go_tree.xpath('//*[@id="search_nature_rg"]/ul/li')
+            #如果有当当自营的书本信息，则写入数据库
+            if go_sort:
+                browser.get(go_sort_href)
+                # go_page = requests.get(url=go_sort_href, headers=headers)
+                go_tree = html.fromstring(browser.page_source)
+                bigimge_li_list = go_tree.xpath('//*[@id="search_nature_rg"]/ul/li')
 
-            #ul标签下的普通商品区域
-            for n,li in enumerate(bigimge_li_list):
-                 #书本详情链接href
-                href = ''.join(li.xpath('./p[@class="name"]/a/@href'))
+                #ul标签下的普通商品区域
+                for n,li in enumerate(bigimge_li_list):
+                     #书本详情链接href
+                    href = ''.join(li.xpath('./p[@class="name"]/a/@href'))
 
-                printDarkBlue(f'{input_key}书本详情链接=====》：{href}\n',FOREGROUND_GREEN)
+                    printDarkBlue(f'{input_key}书本详情链接=====》：{href}\n',FOREGROUND_GREEN)
 
-                #书本封面
-                if len(li.xpath('./a/img/@data-original')) > 0:
-                    src_imag = ''.join(li.xpath('./a/img/@data-original'))
-                else:
-                    src_imag = ''.join(li.xpath('./a/img/@src'))
-                    print('封面图片====》’：', src_imag)
+                    #书本封面
+                    if len(li.xpath('./a/img/@data-original')) > 0:
+                        src_imag = ''.join(li.xpath('./a/img/@data-original'))
+                    else:
+                        src_imag = ''.join(li.xpath('./a/img/@src'))
+                        print('封面图片====》’：', src_imag)
 
 
-                book_inof_sql = f""" replace into book_info(id,src_imag,go_sort_href,href,update_date)
-                     values(REPLACE(UUID(), '-', ''),'{src_imag}','{go_sort_href}','{href}',now())
-                    """
-                print(book_inof_sql)
-                self.sql_cursor.query(book_inof_sql)
-                # time.sleep(1)
-                print('获取详情地址完成！！')
-        else:
-            printDarkBlue(f'抱歉，没有找到与“{input_key}”相关的商品！\n',FOREGROUND_RED)
-            self.get_ddw(self.web_href,self.sql_cursor)
+                    book_inof_sql = f""" replace into book_info(id,src_imag,go_sort_href,href,update_date)
+                         values(REPLACE(UUID(), '-', ''),'{src_imag}','{go_sort_href}','{href}',now())
+                        """
+                    print(book_inof_sql)
+                    self.sql_cursor.query(book_inof_sql)
+                    # time.sleep(1)
+                    print('获取详情地址完成！！')
+            else:
+                printDarkBlue(f'抱歉，没有找到与“{input_key}”相关的商品！\n',FOREGROUND_RED)
+                self.get_ddw(self.web_href,self.sql_cursor)
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
     import codecs
